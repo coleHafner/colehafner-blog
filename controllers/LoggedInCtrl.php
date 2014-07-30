21fcf786ff0d3b978d0ff2f9a6537458bdb4f3a8
cole hafner <colehafner@gmail.com>
2014-07-30 01:28:30 -0700

    logged in application controller now redirects to login instead of index.

--- controllers/LoggedInCtrl.php
+++ controllers/LoggedInCtrl.php
@@ -6,8 +6,11 @@ class LoggedInCtrl extends BaseCtrl {
 	 * @param	Base		$f3
 	 */
 	public function beforeRoute(Base $f3, array $routes, SessionHelper $sh = null) {
-		if (!SessionHelper::create($f3)->isLoggedIn()) {
-			$f3->reroute('/');
+
+		$sh = $sh ? $sh : SessionHelper::create($f3);
+
+		if (!$sh->isLoggedIn()) {
+			$f3->reroute('/login');
 		}
 	}
 }
